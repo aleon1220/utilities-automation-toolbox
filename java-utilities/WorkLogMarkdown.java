aa65bed64110ea84c678445901c28c7e1c0496bc
Andres LeonRangel <aleon1220@outlook.co.nz>
2026-02-10 15:47:04 +1300

    java improvements worklog
    - feat: add week tracking and roll dates to Feb 2026
    - refactor: improve console output and clean up formatting

--- java-utilities/WorkLogMarkdown.java
+++ java-utilities/WorkLogMarkdown.java
@@ -18,39 +18,39 @@ import static java.time.Month.JUNE;
 import static java.time.Month.JULY;
 import static java.time.Month.OCTOBER;
 import static java.time.Month.DECEMBER;
+import java.time.temporal.IsoFields;
+import java.time.LocalDate;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
 
 import static java.util.Map.entry;
 
 class WorkLogMarkdown {
 
-    static LocalDate startDate = LocalDate.of(2026, 1, 19);
-    static LocalDate endDate = LocalDate.of(2026, 1, 31);
+    static LocalDate startDate = LocalDate.of(2026, 2, 10);
+    static LocalDate endDate = LocalDate.of(2026, 2, 28);
 
-    static final Map<LocalDate, String> HOLIDAYS_2026 = Map.ofEntries(
-            entry(LocalDate.of(2026, 1, 1), "New Year's Day"),
+    static final Map<LocalDate, String> HOLIDAYS_2026 = Map.ofEntries(entry(LocalDate.of(2026, 1, 1), "New Year's Day"),
             entry(LocalDate.of(2026, JANUARY, 2), "Day after New Year's Day"),
             entry(LocalDate.of(2026, FEBRUARY, 6), "Waitangi Day"),
             entry(LocalDate.of(2026, APRIL, 3), "Good Friday"),
             entry(LocalDate.of(2026, APRIL, 6), "Easter Monday"),
-            entry(LocalDate.of(2026, APRIL, 27), "Anzac Day (Observed)"), // Actual Saturday Apr 25
+            entry(LocalDate.of(2026, APRIL, 27), "Anzac Day (Observed)"), // Actual Saturday
+                                                                          // Apr 25
             entry(LocalDate.of(2026, JUNE, 1), "King's Birthday"),
             entry(LocalDate.of(2026, JULY, 10), "Matariki"),
             entry(LocalDate.of(2026, OCTOBER, 26), "Labour Day"),
             entry(LocalDate.of(2026, DECEMBER, 25), "Christmas Day"),
-            entry(LocalDate.of(2026, DECEMBER, 28), "Boxing Day (Observed)") // Actual Sat Dec 26
+            entry(LocalDate.of(2026, DECEMBER, 28), "Boxing Day (Observed)") // Actual Sat
+                                                                             // Dec 26
     );
 
-    static Map<LocalDate, String> nzHolidays2025 = Map.of(
-            LocalDate.of(2025, 1, 1), "New Year's Day 2025",
-            LocalDate.of(2025, 1, 2), "Day after New Year's Day Jan 2 2025",
-            LocalDate.of(2025, 2, 6), "Waitangi Day",
-            LocalDate.of(2025, 4, 21), "Easter Monday",
-            LocalDate.of(2025, 4, 25), "ANZAC Day",
-            LocalDate.of(2025, 6, 2), "King's Birthday",
-            LocalDate.of(2025, 6, 20), "Matariki",
-            LocalDate.of(2025, 10, 27), "Labour Day",
-            LocalDate.of(2025, 12, 25), "Christmas Day",
-            LocalDate.of(2025, 12, 26), "Boxing Day");
+    static Map<LocalDate, String> nzHolidays2025 = Map.of(LocalDate.of(2025, 1, 1),
+            "New Year's Day 2025", LocalDate.of(2025, 1, 2), "Day after New Year's Day Jan 2 2025",
+            LocalDate.of(2025, 2, 6), "Waitangi Day", LocalDate.of(2025, 4, 21), "Easter Monday",
+            LocalDate.of(2025, 4, 25), "ANZAC Day", LocalDate.of(2025, 6, 2), "King's Birthday",
+            LocalDate.of(2025, 6, 20), "Matariki", LocalDate.of(2025, 10, 27), "Labour Day",
+            LocalDate.of(2025, 12, 25), "Christmas Day", LocalDate.of(2025, 12, 26), "Boxing Day");
 
     // markdown templates
     public static String markdownWorkLogDayStructure = """
@@ -65,9 +65,9 @@ class WorkLogMarkdown {
             ## MORNING
 
             ### Daily Standup
-            1. ✅ What was done yesterday?
-            2. 🔄 What is planned for today?
-            3. ❗ blockers & escalations
+            1. ✅ What was done yesterday?   **todo_done**
+            2. 🔄 What is planned for today? **todo_planned**
+            3. ❗ blockers & escalations     **todo_blockers**
 
             ### TODO_replace_meeting_name
             1. todo_important_note
@@ -84,7 +84,6 @@ class WorkLogMarkdown {
 
             ### Timesheet submission
             - NZ_Timesheet_Code
-
             """;
 
     static String textFridayTemplate = """
@@ -92,14 +91,13 @@ class WorkLogMarkdown {
             1. reflection
             2.
             3.
-
             """;
 
     void main() {
-        createMarkdownFiles();
-        printDataStructures();
         printHolidays();
         printDaysUntilEndofMonth(LocalDate.now());
+        createMarkdownFiles();
+        printDataStructures();
         // addContent();
     }
 
@@ -198,24 +196,40 @@ class WorkLogMarkdown {
             System.out.printf("%-20s | %-20s%n", date.format(formatter), name);
         });
 
-        System.out.println("----------------------------------------------");
         System.out.println("Total: " + sortedHolidays.size() + " National Holidays");
+        System.out.println("----------------------------------------------");
     }
 
     static void printDaysUntilEndofMonth(LocalDate date) {
         LocalDate endOfMonth = date.withDayOfMonth(date.lengthOfMonth());
         long daysUntilEndOfMonth = ChronoUnit.DAYS.between(date, endOfMonth);
-        System.out.println("======= Days from today until end of Month from " + formatDateForFileName(date) );
-        System.out.println(String.format("%s ", daysUntilEndOfMonth) + "Days till end of the Month ");
-        System.out.println("======= Finishing month of " + date.getMonth() + " Total Days " + daysUntilEndOfMonth);
+        int weekNumber = date.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
+        System.out.println("======= Printing info until End of Month =======");
+        System.out.println("======= " + daysUntilEndOfMonth +
+                " Days from today until end of Month from " + formatDateForFileName(date));
+        System.out.println("======= Current Week Number " + weekNumber);
+        System.out.println("======= " + String.format("%s ", daysUntilEndOfMonth) + " Days until end of Month");
+        System.out.println("======= Finishing month of " + date.getMonth() + " Total remaining Days "
+                + daysUntilEndOfMonth);
+
+        System.out.println("======= Listing remaining days until end of month");
+
+        int lastWeek = -1;
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
 
         for (LocalDate current = date; !current.isAfter(endOfMonth); current = current.plusDays(1)) {
+            int currentWeek = current.get(weekFields.weekOfWeekBasedYear());
             System.out.println(createTitleForWorkLog(current));
-            // System.out.printf(" %n%n%-40s | %s%n", formatDateForFileName(current),
-            // current.getDayOfWeek());
+            
+            if (currentWeek != lastWeek) {
+                System.out.printf("%n======= Header for a new week =======", currentWeek);
+                System.out.printf("%n Week Number %d%n", currentWeek);
+                lastWeek = currentWeek;
+            }
         }
+
         System.out.printf("  %n%n%-40s | %s%n", formatDateForFileName(date), date.getDayOfWeek());
-        System.out.println("================================\n");
+        System.out.println("----------------------------------------------");
     }
 
     static boolean isValidDateRange(LocalDate start, LocalDate end) {
@@ -234,12 +248,14 @@ class WorkLogMarkdown {
 
     static DateValidationResult performDateValidation(LocalDate start, LocalDate end) {
         if (end.isBefore(start)) {
-            return new DateValidationResult(false, "Date Error Validation: endDate cannot be before startDate");
+            return new DateValidationResult(false,
+                    "Date Error Validation: endDate cannot be before startDate");
         }
 
         long daysBetween = ChronoUnit.DAYS.between(start, end);
         if (daysBetween > 30) {
-            return new DateValidationResult(false, "Error: Date range cannot span more than a Month");
+            return new DateValidationResult(false,
+                    "Error: Date range cannot span more than a Month");
         }
 
         return new DateValidationResult(true, "");
