1a7c5ac6133033423be5dc668341594d30a05b11
Andres LeonRangel <aleon1220@outlook.co.nz>
2026-04-07 20:05:51 +1200

    java WorkLog improves lint rules in markdown block

--- java-utilities/lib/src/main/java/org/utils/WorkLogConfig.java
+++ java-utilities/lib/src/main/java/org/utils/WorkLogConfig.java
@@ -45,6 +45,7 @@ public class WorkLogConfig implements Runnable {
     // todo: move to separate class or file if they get more complex to simplify
     public static String markdownWorkLogDayStructure = """
             # {{title_date}}
+            
             ## GOALS
 
             1. Main Planning System
@@ -52,32 +53,42 @@ public class WorkLogConfig implements Runnable {
             3. Client planning system
 
             ## QUESTIONS
+
             1. todo_add_question?
 
             ## MORNING
 
             ### Daily Standup
-            1. ✅ What was done yesterday
-              - **todo_done**
-            2. 🔄 What is planned for today
-              - **todo_planned**
-            3. ❗ blockers & escalations
-              - **todo_add_blocker**
+
+            #### ✅ What was done yesterday
+            - **todo_done**
+            
+            #### 🔄 What is planned for today
+            - **todo_planned**
+            
+            #### ❗ blockers & escalations
+            - **todo_add_blocker**
 
             ## AFTERNOON
+
             ### TODO_activity_name
+
             1. todo_important_note
 
             ## WRAP UP DAY
+
             ## Tasks for next business day
+
             1. todo
             2. todo
 
             ### Day Reflection & Learning
+
             1. todo_add_reflection
             2. todo_add_learning
 
             ### Timesheet submission
+
             - NZ_Timesheet_Code todo_add
 
             """;
